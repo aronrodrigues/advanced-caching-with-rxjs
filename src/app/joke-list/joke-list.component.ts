6df2e377ab5d2afcca4ecc6ee74138f82357acd0
Aron Rodrigues <aronr@amazon.com>
2018-08-15 15:58:02 +0100

    Forcing reload

--- src/app/joke-list/joke-list.component.ts
+++ src/app/joke-list/joke-list.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { Observable, Subject, merge } from "rxjs";
 import { Joke } from "../joke.model";
 import { JokesService } from "../jokes.service";
-import { take, mergeMap, skip, mapTo } from "rxjs/operators";
+import { take, mergeMap, skip, mapTo, switchMap } from "rxjs/operators";
 
 @Component({
   selector: "app-joke-list",
@@ -13,20 +13,31 @@ export class JokeListComponent implements OnInit {
   jokes$: Observable<Array<Joke>>;
   showNotification$: Observable<boolean>;
   update$ = new Subject<void>();
+  forceReload$ = new Subject<void>();
 
   constructor(private jokesService: JokesService) {}
 
   ngOnInit() {
     const initialJokes$ = this.getDataOnce();
-    const updates$ = this.update$.pipe(mergeMap(() => this.getDataOnce()));
+    const reload$ = this.forceReload$.pipe(switchMap(() => this.getNotifications()));
+    const updates$ = merge(this.update$, this.forceReload$).pipe(mergeMap(() => this.getDataOnce()));
     this.jokes$ = merge(initialJokes$, updates$);
-    const initialNotifications$ = this.jokesService.jokes.pipe(skip(1));
-    const show$ = initialNotifications$.pipe(mapTo(true));
-    const hide$ = this.update$.pipe(mapTo(false));
+    const initialNotifications$ = this.getNotifications();
+    const show$ = merge(initialNotifications$, reload$).pipe(mapTo(true));
+    const hide$ = merge(this.forceReload$, this.update$).pipe(mapTo(false));
     this.showNotification$ = merge(show$, hide$);
   }
 
   private getDataOnce() {
     return this.jokesService.jokes.pipe(take(1));
+  }
+
+  getNotifications() {
+    return this.jokesService.jokes.pipe(skip(1));
+  }
+
+  forceReload() {
+    this.jokesService.forceReload();
+    this.forceReload$.next();
   }
 }
--- src/app/jokes.service.ts
+++ src/app/jokes.service.ts
@@ -1,15 +1,14 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { map, shareReplay, switchMap } from "rxjs/operators";
+import { map, shareReplay, switchMap, takeUntil } from "rxjs/operators";
 import { JokeResponse } from "./joke-response.model";
-import { Observable, timer } from "rxjs";
+import { Observable, timer, Subject } from "rxjs";
 import { Joke } from "./joke.model";
 import { environment } from "../environments/environment";
 
-
 @Injectable({ providedIn: "root" })
 export class JokesService {
-
+  private reload$ = new Subject<void>();
   private cache$: Observable<Array<Joke>>;
 
   constructor(private http: HttpClient) {}
@@ -19,6 +18,7 @@ export class JokesService {
       const timer$ = timer(0, environment.refreshInterval);
       this.cache$ = timer$.pipe(
         switchMap(_ => this.requestJokes()),
+        takeUntil(this.reload$),
         shareReplay(environment.cacheSize)
       );
     }
@@ -30,7 +30,12 @@ export class JokesService {
       map(response => {
         console.log(response);
         return response.value;
-      });
-    )
+      })
+    );
+  }
+
+  forceReload() {
+    this.reload$.next();
+    this.cache$ = null;
   }
 }
